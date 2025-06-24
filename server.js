42935ffa72fc89f08012512708dd1a15b1a6781c
govind-mern <govind.desk1493@gmail.com>
2025-06-24 09:15:33 +0530

    adding express

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const http = require('http')
-const server = http.createServer((req,res)=>{
-    if(req.url==='/getData'){
-        res.end('server running')
-    }
-    else
-    res.end('hello world')
+// const http = require('http')
+// const server = http.createServer((req,res)=>{
+//     if(req.url==='/test'){
+//         res.end('server running')
+//     }
+//     else
+//     res.end('hello world')
 
+// })
+// server.listen(3000)
+const express = require('express')
+const app = express()
+
+app.use('/',(req,res)=>{
+    res.send('Hello world')
 })
-server.listen(3000)
+
+app.listen(7777)
